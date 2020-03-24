237f517c70877f4993a3549f2689ab6e0d5f19ed
Sophie Raniwala <Sophie@Sophies-MacBook-Pro.local>
2020-03-24 11:07:14 -0700

    Changes to be committed: 	new file:   rps game.py

---
+++ rps game.py
@@ -0,0 +1,28 @@
+import random
+
+usage = ('Usage Instructions\n  Enter: \n  0 for Rock\n ' \
+                   ' 1 for Paper \n  2 for Scissors')
+print(usage)
+
+while True:
+    
+    user_answer = int(input('Your turn: '))
+    if user_answer < 0 or user_answer > 2:
+        print('Incorrect input. Please enter again.')
+        continue
+
+    computer_answer = random.randint (0, 2)
+
+    print('computer answer: ', computer_answer)
+    
+    # equals = tie
+    if computer_answer == user_answer + 1:
+        print ('computer wins!')
+
+    elif computer_answer == user_answer:
+        print ('tie!')
+
+    else:
+        print ('user wins!')
+
+
