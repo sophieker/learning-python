9069a72299623719540a55f5af70fe91b3751303
Sophie Raniwala <Sophie@Sophies-MacBook-Pro.local>
2020-03-26 14:35:50 -0700

    Changes to be committed: 	new file:   sine_cosine.py
     Changes not staged for commit:
    	modified:   rps game.py

---
+++ sine_cosine.py
@@ -0,0 +1,14 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0,4*np.pi,0.1)   # start,stop,step
+y = np.sin(x)
+z = np.cos(x)
+
+plt.plot(x,y)
+plt.plot(x,z)
+
+plt.xlabel('numbers')
+plt.ylabel('cosine or sine values')
+plt.show()
