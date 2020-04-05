98463481f1173d0c887d1d420a27ae6084fe7ccb
Sophie Raniwala <Sophie@Sophies-MacBook-Pro.local>
2020-04-05 14:08:26 -0700

    Changes to be committed: 	modified:   sticky notes.py

--- sticky notes.py
+++ sticky notes.py
@@ -1,22 +1,47 @@
+#imports
 import tkinter as tk
 from tkinter import *
 from tkinter.font import Font
 
+#beginning questions
 family = (input('What font would you like to use? ') or "Arial")
 nametitle = input('What would you like to title your sticky note? ')
 
+#main window
 root = tk.Tk()
 root.title(nametitle)
-root.geometry("400x400")
+root.geometry("300x300")
 
 c = tk.Canvas(root, width = 300, height = 300)
+
+#font counters
+bold_font_count = 0
+
+#set font
 myFont = Font(family = family, size=12)
+
+#define commands
+def changefont():
+    family = (input('What would you like to change your font to? '))
+    myFont.configure(family = family)
+    
+
 
 def donothing():
    filewin = Toplevel(root)
    button = Button(filewin, text="Do nothing button")
    button.pack()
-   
+
+def boldtext():
+    bold_font_count = bold_font_count + 1
+    if (bold_font_count % 2) == 0:
+        myFont.configure(weight = "normal")
+        t.configure(font = myFont)
+    else:
+        myFont.configure(weight = "bold")
+        t.configure(font = myFont)
+
+#menu bar
 menubar = tk.Menu(root)
 filemenu = Menu(menubar, tearoff = 0)
 filemenu.add_command(label="New", command = donothing)
@@ -46,19 +71,27 @@ helpmenu.add_command(label = "Help Index", command = donothing)
 helpmenu.add_command(label = "About...", command = donothing)
 menubar.add_cascade(label = "Help", menu = helpmenu)
 
+fontmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label = "Font", menu = fontmenu)
+fontmenu.add_command(label = "Bold", command = boldtext)
+fontmenu.add_command(label = "Italic", command = donothing)
+fontmenu.add_command(label = "Size", command = donothing)
+fontmenu.add_command(label = "Underline", command = donothing)
+fontmenu.add_command(label = "Italic", command = donothing)
+fontmenu.add_command(label = "Highlight", command = donothing)
+
+fontmenu.add_separator()
+
+fontmenu.add_command(label = "Change font", command = changefont)
+
+
+
+
 root.config(menu = menubar)
 
+#end and default text
 
-
-def callback():
-    myFont.configure(weight = "bold")
-    t.configure(font = myFont)
-    
-b = tk.Button(root, text="BOLD", command=callback)
-b.pack()
-
-
-t = tk.Text(root, font = myFont, height=30, width=40)
+t = tk.Text(root, font = myFont, height=20, width=40)
 t.insert(INSERT, "Type text here.....")
 t.pack()
 
