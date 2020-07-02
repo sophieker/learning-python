2bcd37e75b77327e2a50757c336c88cbabe71f28
Sophie Raniwala <Sophie@Sophies-MacBook-Pro.local>
2020-07-01 21:27:46 -0700

    yay animation  Changes to be committed: 	modified:   hanui_tower.py

--- hanui_tower.py
+++ hanui_tower.py
@@ -40,69 +40,70 @@ def draw_all_disks(A, B, C):
 # width is the number * 20
 # y position is the position in the array * 20
     for i in range(len(A)):
-        draw_disk(A_x, -83 + i * 20, A[i] * 20, 20, A[i])
+        draw_disk(A_x, -90 + i * 20, A[i] * 20, 20, A[i])
     for i in range(len(B)):
-        draw_disk(B_x, -83 + i * 20, B[i] * 20, 20, B[i])
+        draw_disk(B_x, -90 + i * 20, B[i] * 20, 20, B[i])
     for i in range(len(C)):
-        draw_disk(C_x, -83 + i * 20, C[i] * 20, 20, C[i])
+        draw_disk(C_x, -90 + i * 20, C[i] * 20, 20, C[i])
         
     
 
 def draw_pegs():
     turtle.bgcolor('light gray')
     for i in [A_x, B_x, C_x]:
-        draw_line(i, -93, 90, stick_height, 5, 'black', turtle.getturtle())
+        draw_line(i, -100, 90, stick_height, 5, 'black', turtle.getturtle())
     turtle.update()
 
 def animate_disk(orig_x, orig_y, new_x, new_y, disk_num):
     t = Turtles[disk_num - 1]
-    t.clear()
-    draw_disk(new_x, new_y, disk_num * 20, 20, disk_num)
-"""
-    while y < 200:
-        y = 
-        Turtles[turtle_num].clear()
-        draw_disk(-250, y, )
-        y = y + 0.01
-"""
+
+    for y in range(orig_y, 200, 20):
+        t.clear()
+        draw_disk(orig_x, y, disk_num * 20, 20, disk_num)
+
+    if orig_x > new_x:
+        temp_step = -20
+    else:
+        temp_step = 20
+    for x in range(orig_x, new_x, temp_step):
+        t.clear()
+        draw_disk(x, 200, disk_num * 20, 20, disk_num)
+    for y in range(190, new_y - 1, -20):
+        t.clear()
+        draw_disk(new_x, y, disk_num * 20, 20, disk_num)
+
 def move_disk(begin, finish):
 
     if begin == 'A':
         orig_x = A_x
-        orig_y = -83 + (len(A) - 1) * 20
+        orig_y = -90 + (len(A) - 1) * 20
         disk = A[-1]
         A.pop(-1)
     elif begin == 'B':
         orig_x = B_x
-        orig_y = -83 + (len(B) - 1) * 20
+        orig_y = -90 + (len(B) - 1) * 20
         disk = B[-1]
         B.pop(-1)
     else:
         orig_x = C_x
-        orig_y = -83 + (len(C) - 1) * 20
+        orig_y = -90 + (len(C) - 1) * 20
         disk = C[-1]
         C.pop(-1)
-    print(type(A), type(disk))
         
     if finish == 'A':
         A.append(disk)
         new_x = A_x
-        new_y = -83 + (len(A) - 1) * 20
+        new_y = -90 + (len(A) - 1) * 20
     elif finish == 'B':
         B.append(disk)
         new_x = B_x
-        new_y = -83 + (len(B) - 1) * 20
+        new_y = -90 + (len(B) - 1) * 20
     else:
         C.append(disk)
         new_x = C_x
-        new_y = -83 + (len(C) - 1) * 20
+        new_y = -90 + (len(C) - 1) * 20
 
     animate_disk(orig_x, orig_y, new_x, new_y, disk)
-
-    print('A: ', A)
-    print('B: ', B)
-    print('C: ', C)
-    print('\n')
         
 
 # recursive alg
@@ -124,17 +125,15 @@ turtle.setup(1000,1000)
 turtle.hideturtle()
 turtle.title("Tower of HanUi")
 turtle.speed(0)
-turtle.tracer(4,0)
+turtle.tracer(0,0)
 
-n = 4
+n = 6
 for i in reversed(range(n)):
     # reliance: A is start
     A.append(i + 1)
     t = turtle.Turtle()
     t.hideturtle()
     Turtles.append(t)
-    
-print('Initial value: ' , A)
 
 draw_pegs()
 draw_all_disks(A, B, C)
