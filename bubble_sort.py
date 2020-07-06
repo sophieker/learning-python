48b55c8a531cdc64570d8e5812db764e35c54088
Sophie Raniwala <Sophie@Sophies-MacBook-Pro.local>
2020-07-06 16:09:35 -0700

    Changes to be committed: 	modified:   bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,21 +1,20 @@
-a = [3, 5, 21, 8, 1, 4, 6]
+a = [3, 5, 21, 8, 1, 3]
 
 def sort_arr(arr):
     def switch(x, y):
         arr[x], arr[y] = arr[y], arr[x]
-        print(arr)
 
     n = len(arr)
-    swapped = True
+    switched = True
 
     x = -1
-    while swapped:
-        swapped = False
+    while switched:
+        switched = False
         x = x + 1
+        print(arr)
         for i in range(1, n-x):
             if arr[i - 1] > arr[i]:
                 switch(i-1, i)
-                swapped = True
-
+                switched = True
 
 sort_arr(a)
