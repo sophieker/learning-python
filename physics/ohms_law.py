5ec1ec4d5fd5a2eb01f36a9edd0f55a3d962a409
Sophie Raniwala <Sophie@Sophies-MacBook-Pro.local>
2020-07-11 20:02:40 -0700

    Changes to be committed: 	modified:   physics/ohms_law.py

--- physics/ohms_law.py
+++ physics/ohms_law.py
@@ -1,28 +1,45 @@
-import turtle as t
 import tkinter as tk
 from tkinter import *
-from turtle import *
 import tkinter.font as tkFont
 import math
 import random
 
-# initializing window
+# ---initializing window
 root = tk.Tk()
 root.title("ohm's law simulation")
 root.geometry("800x700")
 
-# font preset
+# ---font preset
 bold_font = tkFont.Font(family="Helvetica", size=30, weight="bold")
 
 c = tk.Canvas(root, width = 800, height = 700)
 
-voltage_ = 0
+voltage_ = 0.0
 resistance_ = 0.1
 
-# update displays
+
+# ---update displays
 def update_battery(voltage):
-    print("Number of batteries: ", math.floor(voltage/1.5))
-    battery_num = math.floor(voltage/1.5)
+    # start batt from: x 180, y 135
+    # batt height: 30
+    # batt width: 50
+    # batt label: 1.5 V
+    battery_num = math.floor(voltage / 1.5)
+    for i in battery_labels:
+        c.delete(i)
+    for i in battery:
+        c.delete(i)
+    # for i in battery_remainder:
+    #    c.delete(i)
+    for i in range(battery_num):
+    #    if voltage%1.5 != 0:
+    #        batt_remainder = c.create_text(245 + i * 51, 130, text=round(voltage%1.5, 2))
+    #        battery_remainder.append(batt_remainder)
+        batt = c.create_rectangle(180 + i * 51, 135, (180 + i * 51) + 50, 165, fill = "white", width=2)
+        battery.append(batt)
+        batt_label = c.create_text(205 + i * 51, 150, text="1.5 V")
+        battery_labels.append(batt_label)
+
     
 def update_resistor(resistance):
     num_dots = math.floor(resistance/5)
@@ -38,10 +55,13 @@ def update_resistor(resistance):
 
 def update_wire(current):
     print("current: ", current)
-    # current_display = c.create_text(370, 300, font=bold_font, text=str(current))
-    # c.pack()
+    for i in current_display:
+        c.delete(i)
+    current_display_temp = c.create_text(400, 320, font=bold_font, text=str(round(current, 2)))
+    current_display.append(current_display_temp)
 
-# update circuit 
+
+# ---update circuit
 def update_voltage(volt_num):
     global voltage_
     print("volt_num: >", volt_num, "<")
@@ -63,7 +83,7 @@ def update_current():
     current = (voltage_ * 1000)/resistance_
     update_wire(current)
     
-# creating sliders
+# ---creating sliders
 volt_num = DoubleVar()
 volt_slider = tk.Scale(root, from_= 0.1, to= 9, resolution=0.1, orient=HORIZONTAL, variable = volt_num, command = update_voltage)
 volt_slider.place(x = 150, y = 600, height = 200, width = 200)
@@ -73,18 +93,21 @@ resistance_slider = tk.Scale(root, from_=10, to=1000, resolution=0.1, orient=HOR
 resistance_slider.place(x = 450, y = 600, height = 200, width = 200)
 
 battery = []
+battery_labels = []
+# battery_remainder = []
 dots = []
-# current_display = c.create_text(370, 300, font=bold_font, text="0")
-# c.pack()
+current_display = []
+current_display_init = c.create_text(400, 320, font=bold_font, text="10.0")
+current_display.append(current_display_init)
 
 
-# labels for sliders
+# ---labels for sliders
 volt_label = tk.Label(root, text = "voltage (V)")
 volt_label.place(x = 140, y = 650)
 resistance_label = tk.Label(root, text = "resistance (Ω)")
 resistance_label.place(x = 440, y = 650)
 
-# draw shapes
+# ---draw shapes
 circuit = c.create_rectangle(150, 150, 650, 500, width = 10)
 c.pack()
 
@@ -101,7 +124,6 @@ update_current()
 root.mainloop()
 
 """
-- draw rectangles (batteries that increase with voltage)
 - display the current
 - toggle show equation
 - white particles to show current
