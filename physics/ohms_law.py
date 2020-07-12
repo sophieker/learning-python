c9ff1f7ed14ce11147bfa3c6b58a2956fec4a60d
Sophie Raniwala <Sophie@Sophies-MacBook-Pro.local>
2020-07-12 11:26:23 -0700

    Changes to be committed: 	modified:   physics/ohms_law.py

--- physics/ohms_law.py
+++ physics/ohms_law.py
@@ -15,7 +15,7 @@ bold_font = tkFont.Font(family="Helvetica", size=30, weight="bold")
 c = tk.Canvas(root, width = 800, height = 700)
 
 voltage_ = 0.0
-resistance_ = 0.1
+resistance_ = 10.0
 
 
 # ---update displays
@@ -82,7 +82,8 @@ def update_current():
     print("update current: ", voltage_, resistance_)
     current = (voltage_ * 1000)/resistance_
     update_wire(current)
-    
+
+
 # ---creating sliders
 volt_num = DoubleVar()
 volt_slider = tk.Scale(root, from_= 0.1, to= 9, resolution=0.1, orient=HORIZONTAL, variable = volt_num, command = update_voltage)
@@ -108,23 +109,26 @@ resistance_label = tk.Label(root, text = "resistance (Ω)")
 resistance_label.place(x = 440, y = 650)
 
 # ---draw shapes
-circuit = c.create_rectangle(150, 150, 650, 500, width = 10)
+circuit = c.create_rectangle(150, 150, 650, 500, width = 15)
 c.pack()
 
 resistance_rect = c.create_rectangle(200, 470, 600, 530, fill= 'white', width=2)
 c.pack()
+
+circuit_units = tk.Label(root, text = "mA", font = ("Helvetica", 16))
+circuit_units.place(x = 385, y = 340)
     
 
 # calculating current
 """
-update_voltage(volt_num)
+update_voltage(volt_num))
 update_resistance(resistance_num)
 update_current()
 """
 root.mainloop()
 
 """
-- display the current
 - toggle show equation
 - white particles to show current
+- increasing size of batteries
 """
